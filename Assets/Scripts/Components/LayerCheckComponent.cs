6c3ce181f3305fc06b8ed8a0a921e5fd4ff14177
Alexandr <rfst@mail.ru>
2025-11-24 22:31:14 +0700

    create hero movement component

---
+++ Assets/Scripts/Components/LayerCheckComponent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class LayerCheckComponent : MonoBehaviour
+    {
+        [SerializeField] private LayerMask layer;
+        [SerializeField] public bool isTouchingLayer;
+        [SerializeField] public Collider2D targetCollider;
+
+        public bool IsTouchingLayer => isTouchingLayer;
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            isTouchingLayer = targetCollider.IsTouchingLayers(layer);
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            isTouchingLayer = targetCollider.IsTouchingLayers(layer);
+        }
+    }
+}
---
+++ Assets/Scripts/Components/MovementComponent.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+namespace Components
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class MovementComponent : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+        [SerializeField] private float jumpForce;
+        [SerializeField] private Rigidbody2D targetRigidbody;
+        [SerializeField] private LayerCheckComponent platformCheck;
+        [SerializeField] private Attractable attractable;
+
+        private Vector2 _direction;
+
+        private void FixedUpdate()
+        {
+            if (_direction == Vector2.zero)
+            {
+                return;
+            }
+
+            DoMove();
+            DoJump();
+        }
+
+        public void SetDirection(Vector2 direction)
+        {
+            _direction = direction;
+        }
+
+        private void DoJump()
+        {
+            var isJumping = _direction.y > 0;
+            if (isJumping && IsOnGround())
+            {
+                targetRigidbody.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+            }
+        }
+
+        private void DoMove()
+        {
+            var attractor = attractable.Attractor;
+            var moveDirection = attractor.Normal.x != 0
+                ? new Vector2(targetRigidbody.velocity.x, _direction.x * attractor.Normal.x * speed)
+                : new Vector2(_direction.x * -attractor.Normal.y * speed, targetRigidbody.velocity.y);
+
+            targetRigidbody.velocity = moveDirection;
+        }
+
+        private bool IsOnGround()
+        {
+            return platformCheck.IsTouchingLayer;
+        }
+    }
+}
